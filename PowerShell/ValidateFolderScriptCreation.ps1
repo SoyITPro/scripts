07b2d5121bf1104436d0f38de257b2f04ae57b9e
SoyITPro <soyitpro@outlook.com>
2025-07-14 11:01:02 -0500

    add folder powershell and files

---
+++ PowerShell/ValidateFolderScriptCreation.ps1
@@ -0,0 +1,19 @@
+$folderPath = "C:\Temp"
+
+#verificar la carpeta
+if (-not (Test-Path -Path $folderPath -PathType Container)) {
+    try {
+        # Crear la carpeta si no existe
+
+        Write-Host "[STATUS] La carpeta $folderPath no existe, creandola..." -ForegroundColor Yellow
+        New-Item -ItemType Directory -Path $folderPath -ErrorAction Stop | Out-Null
+        Write-Host "[SUCCESS] Carpeta creada exitosamente en $folderPath" -ForegroundColor Green
+    }
+
+    catch {
+
+        Write-Host "[ERROR] Fallo al crear la carpeta $folderPath : $_" -ForegroundColor Red
+        exit 1
+    }
+
+}    
